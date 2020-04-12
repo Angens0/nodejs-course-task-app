4b428e3cae0321d028b331b15f2763092246e053
Kamil Sygut <k.sygut@bloomga.com>
2020-04-12 23:06:49 +0200

    76. Inserting Documents

--- mongodb.js
+++ mongodb.js
@@ -11,8 +11,50 @@ const connection = MongoClient.connect(connectionURL, {useNewUrlParser: true, us
 
     const db = client.db(databaseName)
 
-    db.collection('users').insertOne({
-        name: 'Andrew',
-        age: 27
+    // db.collection('users').insertOne({
+    //     name: 'Andrew',
+    //     age: 27
+    // }, (error, result) => {
+    //     if(error) {
+    //         return console.log('Unable to insert user')
+    //     }
+
+    //     console.log(result.ops)
+    // })
+
+    // db.collection('users').insertMany([
+    //     {
+    //         name: 'Jen',
+    //         age: 28
+    //     },
+    //     {
+    //         name: 'Gunther',
+    //         age: 27
+    //     }
+    // ], (error, result) => {
+    //     if(error) return console.log('Unable to insert documents')
+
+    //     console.log(result.ops)
+    // })
+
+    db.collection('tasks').insertMany([
+        {
+            description: 'complete the NodeJS course',
+            completed: false
+        },
+        {
+            description: 'make the bed',
+            completed: true
+        },
+        {
+            description: 'have dinner',
+            completed: true
+        }
+    ], (error, result) => {
+        if(error) {
+            return console.log('Unable to insert documents')
+        }
+
+        console.log(result.ops)
     })
 })
