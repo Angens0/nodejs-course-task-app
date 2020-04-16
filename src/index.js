f8854606778cbfa1d90092cf3794bd2e5021fa06
Kamil Sygut <kamil.patryk.sygut@gmail.com>
2020-04-16 19:55:50 +0200

    91. Resource Reading Endpoints: Part I

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const mongodb = require('mongodb')
 require('./db/mongoose')
 const User = require('./models/User')
 const Task = require('./models/Task')
@@ -14,6 +15,32 @@ app.post('/users', (req, res) => {
         res.status(201).send(user)
     }).catch(error => {
         res.status(400).send(error)
+    })
+})
+
+app.get('/users', (req, res) => {
+    User.find({}).then(users => {
+        res.status(200).send(users)
+    }).catch(error => {
+        res.status(500).send()
+    })
+})
+
+app.get('/users/:id', (req, res) => {
+    const _id = req.params.id
+    
+    if(!mongodb.ObjectID.isValid(_id)) {
+        return res.status(404).send()
+    }
+
+    User.findById(_id).then(user => {
+        if(!user) {
+            return res.status(404).send()
+        }
+
+        res.send(user)
+    }).catch(error => {
+        res.status(500).send()
     })
 })
 
