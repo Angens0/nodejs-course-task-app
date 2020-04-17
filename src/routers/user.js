bafedb2bdf95e87f6591dce97d9005b0ffb77801
Kamil Sygut <kamil.patryk.sygut@gmail.com>
2020-04-17 19:32:42 +0200

    105. Loggin in Users

--- src/routers/user.js
+++ src/routers/user.js
@@ -14,6 +14,15 @@ router.post('/users', async (req, res) => {
     }
 })
 
+router.post('/users/login', async (req, res) => {
+    try {
+        const user = await User.findByCredentials(req.body.email, req.body.password)
+        res.send(user)
+    } catch(error) {
+        res.status(400).send()
+    }
+})
+
 router.get('/users', async (req, res) => {
     try {
         const users = await User.find({})
@@ -62,7 +71,7 @@ router.patch('/users/:id', async (req, res) => {
 
         updates.forEach(update => user[update] = req.body[update])
         await user.save()
-        
+
         if(!user) {
             return res.status(404).send()
         }
