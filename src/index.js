e366b00cdb67475ee3a5a89b89c2916e45a223b2
Kamil Sygut <kamil.patryk.sygut@gmail.com>
2020-04-17 13:29:49 +0200

    100. Resource Deleting Endpoints

--- src/index.js
+++ src/index.js
@@ -79,6 +79,24 @@ app.patch('/users/:id', async (req, res) => {
     }
 })
 
+app.delete('/users/:id', async (req, res) => {
+    const _id = req.params.id
+    if(!mongodb.ObjectID.isValid(_id)) {
+        return res.status(404).send()
+    }
+    
+    try {
+        const user = await User.findByIdAndDelete(_id)
+        if(!user) {
+            return res.status(404).send()
+        }
+
+        res.send(user)
+    } catch(error) {
+        res.status(500).send()
+    }
+})
+
 app.post('/tasks', async (req, res) => {
     const task = new Task(req.body)
 
@@ -138,7 +156,7 @@ app.patch('/tasks/:id', async (req, res) => {
             new: true,
             runValidators: true
         })
-        
+
         if(!task) {
             return res.status(404).send()
         }
@@ -146,6 +164,24 @@ app.patch('/tasks/:id', async (req, res) => {
         res.send(task)
     } catch(error) {
         res.status(400).send(error)
+    }
+})
+
+app.delete('/tasks/:id', async (req, res) => {
+    const _id = req.params.id
+    if(!mongodb.ObjectID.isValid(_id)) {
+        return res.status(404).send()
+    }
+
+    try {
+        const task = await Task.findByIdAndDelete(_id)
+        if(!task) {
+            return res.status(404).send()
+        }
+
+        res.send(task)
+    } catch(error) {
+        res.status(500).send()
     }
 })
 
