31b248f8fae45103073f350f414643dcc25f5d9c
Kamil Sygut <kamil.patryk.sygut@gmail.com>
2020-04-17 23:35:19 +0200

    109. Accepting Authentication Tokens

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const { ObjectID } = require('mongodb')
 const User = require('../models/User')
+const auth = require('../middleware/auth')
 const router = express.Router()
 
 router.post('/users', async (req, res) => {
@@ -25,13 +26,8 @@ router.post('/users/login', async (req, res) => {
     }
 })
 
-router.get('/users', async (req, res) => {
-    try {
-        const users = await User.find({})
-        res.send(users)
-    } catch(error) {
-        res.status(500).send()
-    }
+router.get('/users/me', auth, async (req, res) => {
+    res.send(req.user)
 })
 
 router.get('/users/:id', async (req, res) => {
